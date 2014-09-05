4f56e72a857c865021167cc1efc881959739d539
Stephane Zimmermann <zim.github@gmail.com>
2014-09-05 21:54:29 +0200

    modification du tile editor pour qu'il affiche les tiles dans l'editeur
    Fonctionne presque, demande d'avoir des vraies tiles pour tester (de fixer la taille quoi)

--- Visuals/TileExtender.cs
+++ Visuals/TileExtender.cs
@@ -3,16 +3,30 @@ using System.Collections;
 
 public class TileExtender : MonoBehaviour {
 
+	/// <summary>
+	/// The tile.
+	/// </summary>
 	public Transform tile;
+	private bool drawn = false;
+	private SpriteRenderer rendereur;
+
+	// type des fonctions d'affichage, pour gerer le tiling uniformement
+	private delegate void DrawFunc(Vector3 pos);
 
 	// Use this for initialization
 	void Start () {
 		SpriteRenderer parentRenderer = gameObject.GetComponent<SpriteRenderer>();
 		parentRenderer.enabled = false;
-		FillWithChildren(parentRenderer.bounds.min, parentRenderer.bounds);
+		FillWithChildren(parentRenderer.bounds.min, parentRenderer.bounds, InstantiateTileAt);
 	}
 
-	void FillWithChildren(Vector3 destination, Bounds box){
+	/// <summary>
+	/// Rempli l'objet avec les tiles
+	/// </summary>
+	/// <param name="destination">Position de depart.</param>
+	/// <param name="box">Taille du rectangle a remplir.</param>
+	/// <param name="drawer">Fonction d'affichage.</param>
+	private void FillWithChildren(Vector3 destination, Bounds box, DrawFunc drawer){
 		Bounds bounds = tile.gameObject.GetComponent<SpriteRenderer>().bounds;
 
 		// calcul du nombre d'iterations pour remplir le bloc
@@ -30,16 +44,43 @@ public class TileExtender : MonoBehaviour {
 		Vector3 verticalOffset = new Vector3(0, halfsize.y*2, 0);
 		for(int i=0 ; i<xMaxIteration ; i++){
 			for(int j=0 ; j<yMaxIteration ; j++){
-				Transform newTile = Instantiate(tile) as Transform;
-				newTile.position = destination + halfsize + (i*horizontalOffset) + (j*verticalOffset);
+//				Transform newTile = Instantiate(tile) as Transform;
+				Vector3 position = destination + halfsize + (i*horizontalOffset) + (j*verticalOffset);
+				drawer(position);
 				// nb : on soustrait halfsize pour faire en sorte que les coins inferieurs gauche soient coordonnes
-				newTile.transform.parent = this.transform;
+				//newTile.transform.parent = this.transform;
 			}
 		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	// affichage des tiles dans l'editeur
+	private void OnDrawGizmos(){
+		if(!drawn){
+			rendereur = gameObject.GetComponent<SpriteRenderer>();
+			drawn = true;
+		}
+		FillWithChildren(rendereur.bounds.min, rendereur.bounds, DrawTextureAt);
+
+	}
+
+	/// <summary>
+	/// Fonction d'affichage instanciant les tiles
+	/// </summary>
+	/// <param name="pos">Position ou creer la tile.</param>
+	private void InstantiateTileAt(Vector3 pos) {
+		Transform newTile = Instantiate(tile) as Transform;
+		newTile.position = pos;
+		newTile.transform.parent = this.transform;
+	}
+
+	/// <summary>
+	/// Fonction d'affichage pour l'editeur
+	/// </summary>
+	/// <param name="pos">Position de la tile.</param>
+	private void DrawTextureAt(Vector3 pos){
+		Sprite sprite = tile.gameObject.GetComponent<SpriteRenderer>().sprite;
+		Texture2D texture = sprite.texture;
+		Rect rect = new Rect(pos.x, pos.y, 1, 1);
+		Gizmos.DrawGUITexture (rect, texture);
 	}
 }
